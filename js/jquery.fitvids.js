e4324a442f0fca6b34e337b8b3453db33959cd7d
Scott Nix <scott@scottnix.com>
2012-08-23 14:34:10 -0700

    Updated Fitvids.js script

--- js/jquery.fitvids.js
+++ js/jquery.fitvids.js
@@ -46,9 +46,9 @@
 
     return this.each(function(){
       var selectors = [
-        "iframe[src^='http://player.vimeo.com']",
-        "iframe[src^='http://www.youtube.com']",
-        "iframe[src^='http://www.kickstarter.com']",
+        "iframe[src*='player.vimeo.com']",
+        "iframe[src*='www.youtube.com']",
+        "iframe[src*='www.kickstarter.com']",
         "object",
         "embed"
       ];
@@ -62,16 +62,16 @@
       $allVideos.each(function(){
         var $this = $(this);
         if (this.tagName.toLowerCase() == 'embed' && $this.parent('object').length || $this.parent('.fluid-width-video-wrapper').length) { return; }
-        var height = this.tagName.toLowerCase() == 'object' ? $this.attr('height') : $this.height(),
-            aspectRatio = height / $this.width();
+        var height = ( this.tagName.toLowerCase() == 'object' || $this.attr('height') ) ? $this.attr('height') : $this.height(),
+            width = $this.attr('width') ? $this.attr('width') : $this.width(),
+            aspectRatio = height / width;
         if(!$this.attr('id')){
-            var videoID = 'fitvid' + Math.floor(Math.random()*999999);
-            $this.attr('id', videoID);
+          var videoID = 'fitvid' + Math.floor(Math.random()*999999);
+          $this.attr('id', videoID);
         }
         $this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+"%");
         $this.removeAttr('height').removeAttr('width');
       });
     });
-
   }
 })( jQuery );
