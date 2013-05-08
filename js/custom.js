16fe67179c7221a5828e2548e900353ba21270d4
Scott Nix <scott@scottnix.com>
2013-05-08 12:14:07 -0700

    Added WP jQuery Wrappers
    Didn't cause any issues, but it is just looks better. ;P

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,32 @@
 // apply fitvids functionality to .entry-content div
 // https://github.com/davatron5000/FitVids.js
-jQuery(".entry-content").fitVids();
-
-
+jQuery(document).ready(function($) {
+    $(".entry-content").fitVids();
+});
 // modified version of flexnav, does not have drop down functionality
 // https://github.com/indyplanets/flexnav
 // http://webdeveloper2.com/2011/06/trigger-javascript-on-css3-media-query-change/
 
-detector = jQuery('.js');
-compareWidth = detector.width();
-smallScreen = '768';
+jQuery(document).ready(function($) {
+    detector = jQuery('.js');
+    compareWidth = detector.width();
+    smallScreen = '768';
 
-jQuery('.menu-button').click(function(){
-    if (!jQuery(".access-nav").is(":visible"))
-        jQuery('.menu-button').addClass("nav-open");
-        jQuery(".access-nav").slideToggle('fast', function() {
-    if (!jQuery(".access-nav").is(":visible"))
-        jQuery('.menu-button').removeClass("nav-open");
+    $('.menu-button').click(function() {
+        if (!$(".access-nav").is(":visible"))
+            $('.menu-button').addClass("nav-open");
+            $(".access-nav").slideToggle('slow', function() {
+        if (!$(".access-nav").is(":visible"))
+            $('.menu-button').removeClass("nav-open");
+        });
     });
-});
 
-jQuery(window).resize(function(){
-    if(detector.width()!=compareWidth){
-        compareWidth = detector.width();
-        if (compareWidth >= smallScreen) {
-            jQuery('.access-nav').show();
+    $(window).resize(function(){
+        if (detector.width()!=compareWidth) {
+            compareWidth = detector.width();
+            if (compareWidth >= smallScreen) {
+                $('.access-nav').show();
+            }
         }
-    }
+    });    
 });
